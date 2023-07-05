39958cdd9d68b11ddf24aa69aa450417cb361483
Martin Reyes <martin.anthony.reyes@gmail.com>
2023-07-05 15:30:49 -0500

    wrangling done

--- wrangle.py
+++ wrangle.py
@@ -1,2 +1,50 @@
-def wrangle_zillow():
-    return 0
+import pandas as pd
+
+import sys
+import os
+
+home_directory_path = os.path.expanduser('~')
+sys.path.append(home_directory_path +'/utils')
+
+from acquire_utils import get_connection
+
+def wrangle_zillow_data():
+    '''
+    Acquire zillow data from MySQL database
+    Prepare data by handling missing values and duplicates
+    Save wrangled data into a csv
+    '''
+    # acquire MySQL data
+    df = pd.read_sql('''SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet,
+                            taxvaluedollarcnt, yearbuilt, taxamount, fips 
+                        FROM properties_2017 as p
+                        WHERE p.propertylandusetypeid = 261;''',
+                       get_connection('zillow'))
+    
+    # rename columns
+    df.columns = ['bedrooms', 'bathrooms', 'sqft', 'property_value',
+                  'year_built', 'property_tax', 'county']
+    
+    # filtering data to have "normal" number of bedrooms and bathrooms
+    # 1 to 6 bedrooms
+    df = df[(df['bedrooms'] >= 1) & (df['bedrooms'] <= 6)]
+    # 1 to 6 bathrooms, not including 1.75
+    df = df[(df['bathrooms'] >= 1) & (df['bathrooms'] <= 6) 
+            & (df['bathrooms'] != 1.75)]
+    
+    # handle missing values
+    df = df.dropna()
+    
+    # drop duplicates
+    df = df.drop_duplicates()
+    
+    # giving data appropriate types and values
+    df[['bedrooms', 'sqft', 'property_value', 'year_built']] = \
+        df[['bedrooms', 'sqft', 'property_value', 'year_built']].astype(int)
+    # assigning county codes their respective names
+    df['county'] = df['county'].replace({6037: 'LA', 6059: 'Orange', 6111: 'Ventura'})
+    
+    # cache data
+    df.to_csv('data/zillow_data.csv')
+    
+    return df
